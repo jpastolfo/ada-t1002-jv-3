a095293614dc72ec42560d5946db9398ccbccca6
João Astolfo <joao.astolfo@lnls.br>
2023-09-18 22:53:55 -0300

    18/09/2023 DateTime implementation

--- src/main/java/io/ada/mbnakaya/aula2/Classroom.java
+++ src/main/java/io/ada/mbnakaya/aula2/Classroom.java
@@ -1,8 +1,15 @@
-package main.java.io.ada.mbnakaya.aula2;
+package io.ada.mbnakaya.aula2;
 
-import java.time.ZonedDateTime;
+import org.w3c.dom.ls.LSOutput;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Locale;
 
 /**
@@ -13,6 +20,15 @@ public class Classroom {
 
     public static void main(String[] args) {
         System.out.println("Boa noite!");
+        showDataHora();
+        showParser();
+        showOperations();
+        showChallengeOne();
+        showZonedDateTime();
+        showTemporalAdjusters();
+        showPeriodDurationAndInstant();
+        showChallengeTwo();
+        showDaylightSavings();
     }
 
     /**
@@ -20,8 +36,13 @@ public class Classroom {
      */
     private static void showDataHora() {
         // método now() -> retorna a data/hora atual do relógio do sistema host
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
 
         // método of() -> retorna a data/hora informada
+        System.out.println(LocalDate.of(2023, Month.SEPTEMBER,18));
+        System.out.println(LocalTime.of(10,7,46));
     }
 
     /**
@@ -33,7 +54,16 @@ public class Classroom {
      * Utiliza o método DateTimeFormatter.ofPattern() para sobrescrever o formato de data/hora.
      * Obs: para um objeto de data/hora já instanciado, utilizar o método format().
      */
-    private static void showParser() {}
+    private static void showParser() {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println(now.format(formatter));
+
+        LocalDate date1 = LocalDate.parse("2000-01-01");
+
+        SimpleDateFormat sFormat = new SimpleDateFormat("dd/MM/yyyy");
+        //sFormat.parse("0001/01/01");
+    }
 
     /**
      * EXEMPLOS:
@@ -42,12 +72,21 @@ public class Classroom {
      * minusDays() -> subtrai dias
      * minusDays() -> incrementa dias
      */
-    private static void showOperations() {}
+    private static void showOperations() {
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.println("Dia da semana: " + date.getDayOfWeek());
+        System.out.println("Adicionar horas: " + time.plusHours(2));
+        System.out.println(date.getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("pt","br")));
+    }
 
     /**
      * Challenge One: use o stdout para exibir o dia da semana anterior ao dia do seu nascimento.
      */
-    private static void showChallengeOne() {}
+    private static void showChallengeOne() {
+        System.out.println(LocalDate.of(2000,2,9).minusDays(1).getDayOfWeek());
+    }
 
     /**
      * GMT (Greenwich Mean Time) e o UTC (Universal Time Coordinated) representam o mesmo horário.
@@ -58,7 +97,15 @@ public class Classroom {
      * O ZoneId.of() pode ser utilizado para obter um ZoneId à partir de uma String (precisa ser específica).
      * Obs: consultar a documentação do ZoneId para saber os fusos disponíveis.
      */
-    private static void showZonedDateTime() {}
+    private static void showZonedDateTime() {
+        // Para calcular o fuso, utilizar o now(ZoneId)
+        ZonedDateTime tokyo = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        System.out.println("Horário no Japão: " + tokyo);
+
+        // Para instanciar um horário específico de um fuso, utilizar o of(LocalDateTime.now(),ZoneId)
+        ZonedDateTime tokyo2 = ZonedDateTime.of(LocalDateTime.now(),ZoneId.of("Asia/Tokyo"));
+        System.out.println("Horário no Japão: " + tokyo2);
+    }
 
     /**
      * Utiliza a classe TemporalAdjusters para realizar operações com a data/hora já instanciada.
@@ -67,7 +114,12 @@ public class Classroom {
      * LocalDateTime.with(TemporalAdjusters.next(DayOfWeek)) -> retorna uma cópia do objeto original alterando a data
      * para a próxima sexta-feira.
      */
-    private static void showTemporalAdjusters() {}
+    private static void showTemporalAdjusters() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Agora: " + now);
+        System.out.println("Último dia do mês: " + now.with(TemporalAdjusters.lastDayOfMonth()));
+        System.out.println("Próximo dia da semana: " + now.with(TemporalAdjusters.next(DayOfWeek.SATURDAY)));
+    }
 
     /**
      * Period -> representa dias, meses ou anos
@@ -76,21 +128,39 @@ public class Classroom {
      *
      * Obs: podemos combinar as operações anteriores, como plus(), com as classes de período, duração e instante.
      */
-    private static void showPeriodDurationAndInstant() {}
+    private static void showPeriodDurationAndInstant() {
+        Period period = Period.between(LocalDate.of(2020,1,1),LocalDate.now());
+        System.out.println("Período passado: " + period.toTotalMonths());
+
+        Period twoDays = Period.ofDays(2);
+        System.out.println("Dois dias depois: " + LocalDate.now().plus(twoDays));
+
+        long result = ChronoUnit.MINUTES.between(LocalTime.now(), LocalTime.of(23,10,54));
+        System.out.println("Minutes between: " + result);
+
+    }
 
     /**
      * Utiliza o método ZoneId.getRules().isDaylightSavings(ZonedDateTime.toInstant()) para verificar se o horário
      * foi alterado devido ao horário de verão.
      */
-    private static void showDaylightSavings() {}
+    private static void showDaylightSavings() {
+        ZoneId sp = ZoneId.of("America/Sao_Paulo");
+        boolean isDayLightSavings = sp.getRules().isDaylightSavings(
+                ZonedDateTime.of(LocalDateTime.of(2018,1,1,10,0,0),sp).toInstant());
+
+        System.out.println("É horário de verão? " + isDayLightSavings);
+    }
 
     /**
      * Challenge Two: use o stdout para imprimir a duração de execução do método abaixo.
      */
     private static void showChallengeTwo() {
+        Instant start = Instant.now();
         for (int i = 0; i < 100; i++) {
             System.out.println("Execução de número: " + i);
         }
+        System.out.println("Tempo de execução: " + Duration.between(start,Instant.now()).toMillis() + " ms");
     }
 
     /**
